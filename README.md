855c37e7bce8885e048de683d10550a87b3523ca
ayes-web <ayes2022@protonmail.com>
2023-05-22 19:58:07 +0300

    Added _busy_timeout to docker.nix which should make many containers work together better, added generate-docker-compose.sh with instructions in README.md, small code changes

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Nyaa.si Scraper
 
-nix run .#docker.copyToDockerDaemon
+## Usage guide
+
+1. Fill out $count in `generate-docker-compose.sh`, $count should include how many scrapers you want running
+
+2. Run the script (`./generate-docker-compose.sh`)
+
+It will generate a docker compose file for you into `generated-docker-compose.yml`, rename it to `docker-compose.yml`. Make sure to fill out gluetun fields (its usually VPN credentials). Consult [gluetun documentation](https://github.com/qdm12/gluetun) for further info.
+
+If you dont want to use vpn, which you probably should be using run the script with ``--novpn`` flag (`./generate-docker-compose.sh --novpn`)
+
+
+## Build docker container
+`nix run .#docker.copyToDockerDaemon`
---
+++ generate-docker-compose.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i fish -p fish bat yq
+
+# Config
+# -----
+set count 5 # how many scrapers you want running
+set request_length_ms 700 # Used for calculating estimated completion time
+# -----
+
+if test $(curl "https://nyaa.si/?page=rss" -I | head -n 1 | cut -d ' ' -f2) = 200
+  set max $(curl "https://nyaa.si/?page=rss" | xq ".rss.channel.item[0].link" | sed -e "s/https:\/\/nyaa.si\/download\///" -e "s/[.]torrent//" | tr -d '"')
+else
+  set max 1673894 # default value when actual latest one cant be fetched
+end
+
+set slice $(math floor $max / $count)
+set estimated_hours $(math $max / $count x $request_length_ms / 1000 / 60 / 60)
+argparse 'novpn' -- $argv
+
+# docker compose header
+set compose "version: \"3\""\n"services:"\n
+
+# gluetun entries
+if test not $_flag_novpn
+  for i in $(seq $count)
+      set -a compose "\
+  gluetun$i:
+    image: qmcgaw/gluetun
+    container_name: gluetun$i
+    cap_add:
+      - NET_ADMIN
+    devices:
+      - /dev/net/tun:/dev/net/tun
+    environment:
+      - VPN_SERVICE_PROVIDER=mullvad
+      - VPN_TYPE=wireguard
+      - SERVER_CITIES=Dusseldorf
+      - WIREGUARD_PRIVATE_KEY=
+      - WIREGUARD_ADDRESSES=
+      - TZ=Europe/Tallinn"\n
+  end
+end
+
+# scraper entries
+for i in $(seq $count)
+    set end $(math $i x $slice)
+    set start $(math $end - $slice+1)
+
+    set -a compose "\
+  nyaasi_scraper$i:
+    image: ghcr.io/ayes-web/nyaasi_scraper
+    container_name: nyaasi_scraper$i"\n
+
+    if test not $_flag_novpn
+      set -a compose "   network_mode: service:gluetun$i"\n
+    end
+
+    set -a compose "\
+   command: --start $start --end $end
+    volumes:
+      - ./app:/app/"\n
+end
+
+set tmp $(mktemp)
+echo Estimated completion time: $estimated_hours hours | cat > $tmp
+echo $compose | cat > generated-docker-compose.yml
+
+bat $tmp generated-docker-compose.yml
+rm $tmp
