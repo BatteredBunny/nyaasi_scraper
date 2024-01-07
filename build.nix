a36221fc7446a9bb3e958bfd072be01fee2d31fd
ayes-web <ayes2022@protonmail.com>
2024-01-07 03:39:18 +0200

    Bump versions

--- build.nix
+++ build.nix
@@ -1,11 +1,12 @@
-{ pkgs, buildGoModule, lib }: buildGoModule rec {
-    src = ./.;
+{buildGoModule}:
+buildGoModule {
+  src = ./.;
 
-    name = "github.com/ayes-web/nyaasi_scraper";
-    vendorSha256 = "sha256-Ls0E4VvDu4l9a1n1TO8JL2PKvGE34NVwF9BFA5LwS8g=";
+  name = "github.com/BatteredBunny/nyaasi_scraper";
+  vendorHash = "sha256-HR2fhQ+3IvydezLYFzgmr0adJnAk3PCpPZM9i9yBqkI=";
 
-    ldflags = [
-        "-s"
-        "-w"
-    ];
+  ldflags = [
+    "-s"
+    "-w"
+  ];
 }
